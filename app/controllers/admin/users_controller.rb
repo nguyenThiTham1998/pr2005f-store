8da8b688f29d4253b5504df59ef85d51f1eb297d
nguyenThiTham1998 <nttham98@gmail.com>
2020-10-23 14:33:50 +0700

    Dashboard

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,15 @@
 class Admin::UsersController < Admin::ApplicationController
 
+	def index
+		if params[:val] == "largest"
+			@users = User.all.order_by_orders.paginate(:page => params[:page], :per_page => Settings.limit_user)
+		elsif params[:val] == "oldest"           
+			@users = User.all.paginate(:page => params[:page], :per_page => Settings.limit_user)
+		else 
+			@users = User.all.order_by_time.paginate(:page => params[:page], :per_page => Settings.limit_user)
+		end
+	end	
+
 	def show
 		@user = User.find_by(id: params[:id])
 		if @user.nil?
@@ -11,9 +21,5 @@ class Admin::UsersController < Admin::ApplicationController
 			@type = params[:type]
 		end 
 	end
-	
-	def index
-		@users = User.all.order_by_time.paginate(:page => params[:page], :per_page => Settings.limit_user)	
-	end	
 
 end	
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,8 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = {host: "localhost", port: 3000}
   config.after_initialize do
-    Bullet.enable = true
-    Bullet.alert = true
+    
 
   end
 
